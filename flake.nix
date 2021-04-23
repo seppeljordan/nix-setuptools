b88c27ce93ab2918134142f4ef9393938a40596b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-04-23 12:56:06 +0200

    Move parser tests to tests subdirectory

--- flake.nix
+++ flake.nix
@@ -10,15 +10,28 @@
   outputs = { self, nix-parsec, flake-utils, nixpkgs }:
     let
       systemDependent = flake-utils.lib.eachDefaultSystem (system:
-        let pkgs = import nixpkgs { inherit system; };
-        in {
-          devShell = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; };
-          checks = import ./tests.nix {
+        let
+          pkgs = import nixpkgs { inherit system; };
+          callFunction = callFunctionWith {
             inherit (self.lib) setuptools;
             inherit (pkgs) writeTextFile;
             inherit (nixpkgs) lib;
+            inherit callFunctionWith;
           };
+        in {
+          devShell = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; };
+          checks = callFunction ./tests/test-parser.nix { };
         });
+      callFunctionWith = autoArguments: functionOrPath: arguments:
+        let
+          f = if nixpkgs.lib.isFunction functionOrPath then
+            functionOrPath
+          else
+            import functionOrPath;
+          functionArguments = nixpkgs.lib.functionArgs f;
+        in f (builtins.intersectAttrs functionArguments
+          (autoArguments // arguments));
+
     in {
       lib = {
         setuptools = import ./setuptools.nix {
